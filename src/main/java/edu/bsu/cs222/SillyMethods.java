4636b0b31611abaafa0c888ce6c36a7edfea54e2
angelesmarin <angeles.marinbatana@bsu.edu>
2023-09-07 08:54:54 -0400

    initial commit

--- src/main/java/edu/bsu/cs222/SillyMethods.java
+++ src/main/java/edu/bsu/cs222/SillyMethods.java
@@ -1,9 +1,17 @@
 package edu.bsu.cs222;
-
-//SillyMethods = LeapYearCalculator
-//didGuess42 = isLeapYear
 public class SillyMethods {
     public boolean didGuess42(int year) {
         return true;
+    }
+
+    public String countTo(int num) {
+        StringBuilder builder = new StringBuilder();
+        for (int number = 0; number <= num; number++) {
+            builder.append(number).append(" ");
+        }
+        String result = builder.toString().trim();
+        return result;
+
+
     }
 }
